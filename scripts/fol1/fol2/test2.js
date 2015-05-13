85c134ef8baacee931a92746c3e9c641939cc7fb
Walid Choufani <walidchoufani@users.noreply.github.com>
2015-05-13 15:53:26 +0300

    new content

--- scripts/fol1/fol2/test2.js
+++ scripts/fol1/fol2/test2.js
@@ -1,7 +1,14 @@
-return 3;
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var x =7;
 /*
 this is a comment
 */
+return x;
 /**
 and another one
 **/
+   							
